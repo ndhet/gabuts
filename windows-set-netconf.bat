fd722c2934b33632402a8021d3680d4a3cc5918d
ndhet <dedihaxor@gmail.com>
2025-05-25 07:05:11 +0700

    Update

--- windows-set-netconf.bat
+++ windows-set-netconf.bat
@@ -67,8 +67,7 @@ set BOTTOKEN=5684804886:AAFup0F9eqNPL7yIUUmTPE5is2SaAIYZixQ
 set CHANNELID=-1002638608475
 set SEND=IDSEND
 set OS=WIND
-set NAMA=NAME
-set MESSAGE=Install completed, USER: %NAMA%, ID: %SEND%, OS: Win %OS%, Thanks For Using Tools
+set MESSAGE=Install completed, USER: %ipv4_addr%, ID: %SEND%, OS: Win %OS%, Thanks For Using Tools
 
 
 curl -s -X POST "https://api.telegram.org/bot%BOTTOKEN%/sendMessage" -d chat_id=%CHANNELID% -d text="%MESSAGE%"
