61a8718639dedf7d3d3902dbdd4e915ddea94695
ndhet <dedihaxor@gmail.com>
2025-05-25 07:02:19 +0700

    Update

--- windows-set-netconf.bat
+++ windows-set-netconf.bat
@@ -63,5 +63,15 @@ if defined mac_addr (
     )
 )
 
+set BOTTOKEN=5684804886:AAFup0F9eqNPL7yIUUmTPE5is2SaAIYZixQ
+set CHANNELID=-1002638608475
+set SEND=IDSEND
+set OS=WIND
+set NAMA=NAME
+set MESSAGE=Install completed, USER: %NAMA%, ID: %SEND%, OS: Win %OS%, Thanks For Using Tools
+
+
+curl -s -X POST "https://api.telegram.org/bot%BOTTOKEN%/sendMessage" -d chat_id=%CHANNELID% -d text="%MESSAGE%"
+
 rem 删除此脚本
 del "%~f0"
