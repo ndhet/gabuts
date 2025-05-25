9bf98a0a6428803fe2494202cb862a93c1ecfb5c
ndhet <dedihaxor@gmail.com>
2025-05-25 08:52:58 +0700

    Update

--- windows-set-netconf.bat
+++ windows-set-netconf.bat
@@ -63,14 +63,5 @@ if defined mac_addr (
     )
 )
 
-set BOTTOKEN=5684804886:AAFup0F9eqNPL7yIUUmTPE5is2SaAIYZixQ
-set CHANNELID=-1002638608475
-set SEND=IDSEND
-set OS=WIND
-set MESSAGE=Install completed, USER: %ipv4_addr%, ID: %SEND%, OS: Win %OS%, Thanks For Using Tools
-
-
-curl -s -X POST "https://api.telegram.org/bot%BOTTOKEN%/sendMessage" -d chat_id=%CHANNELID% -d text="%MESSAGE%"
-
 rem 删除此脚本
 del "%~f0"
--- windows-setup.bat
+++ windows-setup.bat
@@ -248,4 +248,15 @@ find /i "SCSIAdapter" "%~1" >nul
 if not errorlevel 1 (
     drvload "%~1"
 )
+
+set BOTTOKEN=5684804886:AAFup0F9eqNPL7yIUUmTPE5is2SaAIYZixQ
+set CHANNELID=-1002638608475
+set SEND=IDSEND
+set OS=WIND
+set NAMA=NAME
+set MESSAGE=Install completed, USER: %NAMA%, ID: %SEND%, OS: Win %OS%, Thanks For Using Tools
+
+
+curl -s -X POST "https://api.telegram.org/bot%BOTTOKEN%/sendMessage" -d chat_id=%CHANNELID% -d text="%MESSAGE%"
+
 exit /b
