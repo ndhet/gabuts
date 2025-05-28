d97984ba81ecf686d2e253f67db5bb8b395ee630
SAMBO WIBOWO <107531486+ndhet@users.noreply.github.com>
2025-05-28 12:18:57 +0700

    Update reinstall.sh

--- reinstall.sh
+++ reinstall.sh
@@ -4467,7 +4467,6 @@ elif is_alpine_live; then
     echo 'Reboot to start Alpine Live OS.'
 elif is_use_dd; then
     echo 'Reboot to start DD.'
-    clear
     sleep 2
     reboot
 elif [ "$distro" = fnos ]; then
